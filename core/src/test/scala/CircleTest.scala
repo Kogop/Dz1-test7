a5384baadc7f06f61325ad03da5bc980b3cec313
Vladislav Nesterov <nesterovvlad100@gmail.com>
2021-12-03 20:46:39 +0300

    nemnogo sdelal po krasivee, i hotya bi dlya vida dobavil vechno ischezaushuu strochku. import org.scalatest._   . Vot etu

--- core/src/test/scala/CircleTest.scala
+++ core/src/test/scala/CircleTest.scala
@@ -1,4 +1,4 @@
-
+//import org.scalatest._
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
 
@@ -9,7 +9,7 @@ class CircleTest extends AnyFlatSpec with should.Matchers {
   "CircleRound" should "calculate area for given Circle data" in {
     val rd = CircleSquare(2)
 
-    victim().RoundC(rd) should be (2*2*Math.PI)
+    victim().RoundC(rd) should be (2 * 2 * Math.PI)
   }
 
   it should "return zero if Round is zero" in {
